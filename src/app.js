c31285a908040c6085981ee7668efda4e035b82e
DIANA CAMPOS DEL CARPIO <201110624@urp.edu.pe>
2024-10-23 04:09:57 -0500

    Subida de Modelo ROL

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const conectarDB = require('./config/database');
 const authRoutes = require('./routes/auth');
 const areaRoutes = require('./routes/areas');
+const rolRoutes = require('./routes/roles');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors'); 
@@ -45,6 +46,23 @@ const swaggerOptions = {
               description: 'Descripción del área'
             }
           }
+        },
+        Rol: { // Agregar la definición del esquema Rol
+          type: 'object',
+          properties: {
+            nombre: {
+              type: 'string',
+              description: 'Nombre del rol'
+            },
+            descripcion: {
+              type: 'string',
+              description: 'Descripción del rol'
+            },
+            area: {
+              type: 'string',
+              description: 'ID del área asociada al rol'
+            }
+          }
         }
       }
     },
@@ -61,6 +79,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // Rutas de la API
 app.use('/auth', authRoutes);
 app.use('/areas', areaRoutes);
+app.use('/roles', rolRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
