da16c5ed9f60361dc156f25e14986c1381d602c1
DIANA CAMPOS DEL CARPIO <201110624@urp.edu.pe>
2024-11-12 00:40:12 -0500

    Cambio de titulo

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'SYSFUN-BACKEND APP',
+      title: 'SYSFUN-BACKEND APP TESIS URP',
       version: '1.0.0',
     },
     components: { 
